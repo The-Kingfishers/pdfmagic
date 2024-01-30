6c48be1d64c71f6affd8233afe6d3bc169b3d736
Jahid2121 <138579420+Jahid2121@users.noreply.github.com>
2024-01-30 09:32:40 +0600

    Created Staging branch

---
+++ src/app/services/page.jsx
@@ -0,0 +1,40 @@
+"use client"
+
+import useServices from "@/hooks/useServices";
+import Container from "../utils/Container";
+import ServicesCard from "./servicesCard/ServicesCard";
+
+
+const Services = () => {
+    const [services] = useServices()
+    console.log(services);
+  
+
+    return (
+        <div >
+            <Container>
+                <h1 className="text-center my-16 font-bold text-2xl">
+                    Every tool you need to work with PDFs in one place
+                    Every tool you need to use PDFs, at your fingertips. All are 100% FREE and easy to use! Merge, split, compress, convert, rotate, unlock and watermark PDFs with just a few clicks.
+                    <h3>Staging branch created</h3>
+                </h1>
+
+                <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-2 my-4">
+                    {
+                        services?.map(service => (
+                            <ServicesCard
+                                key={service._id}
+                                image={service.image}
+                                name={service.name}
+                                feature={service.feature}
+                                benefit={service.benefit}>
+                            </ServicesCard>)
+                        )
+                    }
+                </div>
+            </Container>
+        </div>
+    );
+};
+
+export default Services;
