566911a2ccef3f3895c4a18115f7e21db7306266
Jahid2121 <138579420+Jahid2121@users.noreply.github.com>
2024-02-10 12:43:48 +0600

    Add Every Service page Dynamically based user clicks on service

--- src/app/components/ConverterPage/Converter.jsx
+++ src/app/components/ConverterPage/Converter.jsx
@@ -4,6 +4,14 @@ import { usePathname } from "next/navigation";
 import { useState } from "react";
 import JpgToPdf from "../JpgToPdf";
 import WordToPdf from "../WordToPdf";
+import MergePdf from "../MergePdf";
+import PdfToPowerPoint from "../PdfToPowerPoint";
+import PageNumbering from "../PageNumbering";
+import Pdf2Word from "../Pdf2Word";
+import Pdf2Excel from "../Pdf2Excel";
+import Excel2Pdf from "../Excel2Pdf";
+import Pdf2Jpg from "../Pdf2Jpg";
+import Sign2Pdf from "../Sign2Pdf";
 
 const Converter = ({ File, conversionType }) => {
     console.log(conversionType);
@@ -23,6 +31,22 @@ const Converter = ({ File, conversionType }) => {
             return <WordToPdf />
         case "jpgtopdf":
         return <JpgToPdf url={url} />
+        case "mergePdf":
+        return <MergePdf url={url} />
+        case "PdfToPowerPoint":
+        return <PdfToPowerPoint url={url} />
+        case "pageNumbering":
+        return <PageNumbering url={url} />
+        case "pdf2Word":
+        return <Pdf2Word url={url} />
+        case "pdf2Excel":
+        return <Pdf2Excel url={url} />
+        case "excel2Pdf":
+        return <Excel2Pdf url={url} />
+        case "pdf2Jpg":
+        return <Pdf2Jpg url={url} />
+        case "sign2Pdf":
+        return <Sign2Pdf url={url} />
     }
 
 };
